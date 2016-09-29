416b6e81f67f598a4e3b020e3fc9b220c73df834
vectorxu <991450259@qq.com>
2016-09-29 11:43:08 +0800

    first commit

---
+++ wordcount.py
@@ -0,0 +1,20 @@
+import os
+from operator import add
+os.environ['SPARK_HOME'] = 'E:\hadoopsoftware\spark-1.4.0-bin-hadoop2.6'
+from pyspark import SparkContext, SparkConf
+
+appName = "worldcount"
+master = "local"
+conf = SparkConf().setAppName(appName).setMaster(master)
+sc = SparkContext(conf=conf)
+
+lines = sc.textFile("d:\\spark.txt")
+
+counts = lines.flatMap(lambda x: x.split('\t')) \
+                  .map(lambda x: (x, 1)) \
+                  .reduceByKey(add)
+output = counts.collect()
+for (word, count) in output:
+    print("%s: %i" % (word, count))
+
+sc.stop()
